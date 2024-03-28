53b3afc07bac02b9e7e79aa1c20108e738455775
Sachinku94 <sachinku94@gmail.com>
2024-03-28 23:13:21 +0530

    Code Corrections

--- OncWeekly/test_HeaderOW.py
+++ OncWeekly/test_HeaderOW.py
@@ -31,11 +31,11 @@ class TestOne(BaseClass):
 
     def test_sociallink(self):
         sociallinks=[]
-        reversedsociallinks=[]
-        sociallinkssrc=[]
+        
         allsociallinks=[]
         sociallinks.append('https://oncweekly.com/?llldldldl')
         pw=self.driver.find_element(By.XPATH,"//div/figure/a")
+        assert pw.is_displayed
         pwlink=pw.get_attribute('href')
         sociallinks.append(pwlink)
         AC=ActionChains(self.driver)
@@ -44,24 +44,28 @@ class TestOne(BaseClass):
 
         social_links=self.driver.find_elements(By.XPATH,"//div[@class='social-icons']/ul/li/a")
         for lik in social_links:
+            assert lik.is_displayed
             li=lik.get_attribute('href')
+
             sociallinks.append(li)
             AC.key_down(Keys.CONTROL).click(lik).key_up(Keys.CONTROL).perform()
         sub=self.driver.find_element(By.XPATH,"//a[@id='top-subscribe']")
+        assert sub.is_displayed
+        subhr=sub.get_attribute('href')
+        sociallinks.append(subhr)
         AC.key_down(Keys.CONTROL).click(sub).key_up(Keys.CONTROL).perform()
         
-        for i in range(1, len(sociallinks) + 1):
-              reversedsociallinks.append(sociallinks[-i])
+        
         
         handles =self.driver.window_handles
 
-        for windows in handles[1:]:
+        for windows in handles:
             self.driver.switch_to.window(windows)
             alllinks=self.driver.current_url
             allsociallinks.append(alllinks)
-
-        assert reversedsociallinks==allsociallinks
         
+
+        assert set(sociallinks)==set(allsociallinks)
 
 
 
